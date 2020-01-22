b5e5c71153135986947b9da129285819108cd61b
Arseniy Ivanov <freeatnet@freeatnet.com>
2020-01-22 18:32:39 +0800

    Remove keys when their values are empty

--- scripts/entry_io.py
+++ scripts/entry_io.py
@@ -13,7 +13,10 @@ def write_token_entry(address, listing):
     with open("tokens/{}.yaml".format(address), "w") as outfile:
         outfile.write(
             yaml.dump(
-                dict(address=address, **listing),
+                dict(
+                    address=address,
+                    **{k: v
+                       for (k, v) in listing.items() if v}),
                 explicit_start=True,
                 width=YAML_WIDTH,
                 indent=YAML_INDENT,
